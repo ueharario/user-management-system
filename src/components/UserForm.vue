f9f611f569ff7e29c64495351105dc4942c909c2
ueharario <uehara@backham.me>
2022-09-14 16:58:00 +0900

    watch の修正(editName に newValue を代入)

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -34,7 +34,7 @@ export default {
             () => this.name,
             (newValue, oldValue) => {
                 if (newValue !== oldValue) {
-                    this.editName = this.name
+                    this.editName = newValue
                 }
             },
             {
