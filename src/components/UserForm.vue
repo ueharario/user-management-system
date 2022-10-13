735c1eed8638ffc7985a828948d885cc45bdf698
ueharario <uehara@backham.me>
2022-10-13 16:34:06 +0900

    fix : InputField ファイルの作成 - issues#25

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -31,10 +31,10 @@
 </template>
 
 <script>
-import InputTextName from '@/components/InputTextName.vue'
-import SelectItem from '@/components/SelectItem.vue'
-import InputTextMaleMsg from '@/components/InputTextMaleMsg.vue'
-import InputTextFemaleMsg from '@/components/InputTextFemaleMsg.vue'
+import InputTextName from '@/components/InputField/InputTextName.vue'
+import SelectItem from '@/components/InputField/SelectItem.vue'
+import InputTextMaleMsg from '@/components/InputField/InputTextMaleMsg.vue'
+import InputTextFemaleMsg from '@/components/InputField/InputTextFemaleMsg.vue'
 import { GENDER, GENDER_ARRAY, DEFAULT_USER, TITLE } from '@/constants/USERS.js'
 import * as yup from "yup";
 
